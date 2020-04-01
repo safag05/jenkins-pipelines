67483b818de111b52f0a6e9b4646e3d28482a3bb
Safa Gumuscibik <safagumus05@gmail.com>
2020-03-31 21:23:06 -0400

    updates

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -1,25 +1,32 @@
 node {
-    properties([
-        // Below Line sets "Discard Builds more than 5"
-        buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
-        
-        // Below Line triggers this job every minute
-        pipelineTriggers([pollSCM('* * * * *')])
-        ])
-
-stage("Pull Repo"){ 
-git   'https://github.com/farrukh90/cool_website.git' 
-} 
-stage("Stage2"){ 
-echo "hello" 
-} 
-stage("Stage3"){ 
-echo "hello" 
-} 
-stage("Stage4"){ 
-echo "hello" 
-} 
-stage("Stage5"){ 
-echo "hello" 
-} 
-} 
+	properties([
+		// Below line sets "Discard Builds more than 5"
+		buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
+		
+		// Below line triggers this job every minute
+		pipelineTriggers([pollSCM('* * * * *')])
+		])
+	stage("Pull Repo"){
+		git   'https://github.com/farrukh90/cool_website.git'
+	}
+	stage("Install Prerequisites"){
+		sh """
+		ssh centos@jenkins_worker1.acirrustech.com                 sudo yum install httpd -y
+		"""
+	}
+	stage("Copy artifacts"){
+		sh """
+		scp -r *  centos@jenkins_worker1.acirrustech.com:/tmp
+		ssh centos@jenkins_worker1.safa-g.com                 sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@jenkins_worker1.safa-g.com                 sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@jenkins_worker1.safa-g.com				   sudo chown centos:centos /var/www/html/
+		ssh centos@jenkins_worker1.safa-g.com				   sudo chmod 777 /var/www/html/*
+		"""
+	}
+	stage("Restart web server"){
+		sh "ssh centos@jenkins_worker1.safa-g.com                 sudo systemctl restart httpd"
+	}
+	stage("Slack"){
+		slackSend color: '#BADA55', message: 'Hello, World!'
+	}
+}
