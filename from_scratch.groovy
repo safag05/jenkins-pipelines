202b786d2476263cb0dd8ac15475b1e7f7ab524c
Safa Gumuscibik <safagumus05@gmail.com>
2020-03-31 21:39:30 -0400

    updates

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -13,30 +13,33 @@ node {
 			description: 'Please choose an environment', 
 			name: 'ENVIR')]), 
 		])
+
+		// Pulls a repo from developer
 	stage("Pull Repo"){
 		git   'https://github.com/farrukh90/cool_website.git'
 	}
-        //Installs web server on different environment
+		//Installs web server on different environment
 	stage("Install Prerequisites"){
 		sh """
-		ssh centos@jenkins_worker1.${ENVIR}.com                 sudo yum install httpd -y
+		ssh centos@${ENVIR}                 sudo yum install httpd -y
 		"""
 	}
-        //Copies over developers files to different environment
+		//Copies over developers files to different environment
 	stage("Copy artifacts"){
 		sh """
-		scp -r *  centos@jenkins_worker1.${ENVIR}.com:/tmp
-		ssh centos@jenkins_worker1.${ENVIR}.com                 sudo cp -r /tmp/index.html /var/www/html/
-		ssh centos@jenkins_worker1.${ENVIR}.com                 sudo cp -r /tmp/style.css /var/www/html/
-		ssh centos@jenkins_worker1.${ENVIR}.com				   sudo chown centos:centos /var/www/html/
-		ssh centos@jenkins_worker1.${ENVIR}.com				   sudo chmod 777 /var/www/html/*
+		scp -r *  centos@${ENVIR}:/tmp
+		ssh centos@${ENVIR}                 sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@${ENVIR}                 sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@${ENVIR}				    sudo chown centos:centos /var/www/html/
+		ssh centos@${ENVIR}				    sudo chmod 777 /var/www/html/*
 		"""
 	}
-        //Restarts web server
+		//Restarts web server
 	stage("Restart web server"){
-		sh "ssh centos@jenkins_worker1.${ENVIR}.com                 sudo systemctl restart httpd"
+		sh "ssh centos@${ENVIR}                 sudo systemctl restart httpd"
 	}
-        //Sends a message to slack
+
+		//Sends a message to slack
 	stage("Slack"){
 		slackSend color: '#BADA55', message: 'Hello, World!'
 	}
