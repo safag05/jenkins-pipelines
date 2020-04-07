c915637dc7d0a5b341bb752986fa1d80c8a7cd87
Safa Gumuscibik <safagumus05@gmail.com>
2020-04-07 19:52:09 -0400

    updated version

--- artemis_delivery.groovy
+++ artemis_delivery.groovy
@@ -28,7 +28,7 @@ timestamps {
 
 ws { 
 
-checkout([$class: 'GitSCM', branches: [[name: 'dev']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/fuchicorp/artemis.git']]]) 
+checkout([$class: 'GitSCM', branches: [[name: '${Version}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/fuchicorp/artemis.git']]]) 
 
 } 
 
